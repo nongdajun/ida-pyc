c9b594ef3a664ee0d7155d50e6d27f4c052610de
nongdajun <ndj8886@163.com>
2025-04-07 17:52:17 +0800

    fix mistake

--- procs/pyc-proc.py
+++ procs/pyc-proc.py
@@ -9,7 +9,7 @@ def PROCESSOR_ENTRY():
     sys.path.append(f"{os.path.dirname(__file__)}/pyc-procs")
 
     tuple_version, is_pypy = pyc_info[0], pyc_info[4]
-    if tuple_version[0]==31:
+    if tuple_version[0]==3:
         m = __import__('pyc_v3')
     else:
         m = __import__('pyc_simple')
